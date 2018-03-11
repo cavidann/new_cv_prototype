68d7a94b9530701a69601f5e1e6204667a4fc47e
cavidann <cavi98@icloud.com>
2018-03-12 05:21:58 +0600

    porftolio, responsiveVoice & some designs added

--- js/main.js
+++ js/main.js
@@ -4,7 +4,25 @@ $(document).ready(function () {
             var a = $(this).text();
             var b = $(this).parent().parent().last().find(".progress_main").css({ "width": a });
         })
-        console.log("1")
     })
 
+    $(".fa-pause").on("click", function () {
+        EndCallback();
+        responsiveVoice.speak("", "UK English Male", { onstart: StartCallback, onend: EndCallback });
+    })
+
+    $(".fa-play").on("click", function () {
+        $(".fa-play").css({"display":"none"})
+        $(".fa-pause").css({"display":"inline-block"})
+            responsiveVoice.setDefaultVoice("US English Male");
+            responsiveVoice.speak("I can handle uninterrupted", "UK English Male", { onstart: StartCallback, onend: EndCallback });
+
+    });
+
+    function StartCallback() {}
+
+    function EndCallback() {
+        $(".fa-play").css({"display":"inline-block"})
+        $(".fa-pause").css({"display":"none"})
+    }
 });
